3f2b8d38cadd6783dd5748621be74f4b74a9a511
LAPTOP-RFDL1QJT\ASUS <idinmaster.1004.2015@gmail.com>
2024-03-26 16:06:40 +0700

    Fix config

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
     images: {
         domains: ['drive.google.com']
     },
-    env: {
-        FRONTEND_URL: process.env.FRONTEND_URL,
-        BACKEND_URL: process.env.BACKEND_URL
-    },
     async headers() {
         return [
             {
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { useRef, useState, useEffect} from "react"
-import userRegister from "@/libs/userRegister";
 
 export default function Register() {
     const firstname = useRef("");
