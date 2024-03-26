a3cdc384cb1f2ed8c0730e0420dec5f84c507ca3
Copy <masterkcopyk@gmail.com>
2024-03-26 20:20:43 +0700

    change pic

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import { useSession } from 'next-auth/react';
 
 export default function Banner () {
-    const covers = ['/img/cover.jpg','/img/cover2.jpg','/img/cover3.jpg','/img/cover4.jpg']
+    const covers = ['/img/cover1.jpg','/img/cover2.jpg','/img/cover3.jpg','/img/cover4.jpg']
     const [index, setIndex] = useState(0)
     const router = useRouter()
 
