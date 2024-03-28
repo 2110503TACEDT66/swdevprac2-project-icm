224b60f332b2c831cf33994ee2b74ada87dca88f
LAPTOP-RFDL1QJT\ASUS <idinmaster.1004.2015@gmail.com>
2024-03-28 16:23:03 +0700

    Fix dentist page

--- src/app/(dentistinfo)/dentist/page.tsx
+++ src/app/(dentistinfo)/dentist/page.tsx
@@ -3,8 +3,8 @@ import DentistCatalog from "@/components/DentistCatalog"
 import { Suspense } from "react"
 import { LinearProgress } from "@mui/material"
 
-export default function Dentist() {
-    const dentists = getDentists()
+export default async function Dentist() {
+    const dentists = await getDentists()
     return (
         <main className="text-center p-5">
             <h1 className="text-3xl font-medium text-black">Select Your Dentist</h1>
